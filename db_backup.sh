892802aa1787277ace02312c29c9a86d5ec1ff04
ak12a <arvindrhce12@gmail.com>
2021-07-09 17:32:24 +0530

    removed cred

--- db_backup.sh
+++ db_backup.sh
@@ -4,13 +4,13 @@ BACKUP=db-$DATE.sql
 HOST=$1
 DB_PASS=$2
 DB_NAME=$3
-AWS_SEC=$4
-BUCKET_NAME=$5
+AWS_ACCESS=$4
+AWS_SEC=$5
+BUCKET_NAME=$6
 	
 mysqldump -u root -h $HOST -p$DB_PASS $DB_NAME > /tmp/$BACKUP && \
 
-export AWS_ACCESS_KEY_ID=AKIAWQNGCN6FVXXOHRH4 && \
-#export AWS_SECRET_ACCESS_KEY=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 && \
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SEC  && \
 
 echo " uploading dabase backup $BACKUP to aws s3 bucket $BUCKET_NAME"
